11bd72a23e00d6d76817c869a589da6170466c80
Bronepuh <darckday2007@mail.ru>
2021-01-20 00:49:12 +0300

    fixed bugs

--- build/js/main.js
+++ build/js/main.js
@@ -2,11 +2,15 @@
 (function () {
   var pageHeader = document.querySelector('.page-header');
   var headerToggle = document.querySelector('.page-header__toggle');
+  var menuItems = document.querySelectorAll('.navigation__item');
 
   pageHeader.classList.remove('page-header--nojs');
   pageHeader.classList.add('page-header--closed');
 
   if (headerToggle) {
     headerToggle.addEventListener('click', window.vendor.togglePageHeader);
+    for (var i = 0; i < menuItems.length; i++) {
+      menuItems[i].addEventListener('click', window.vendor.closeMenu);
+    }
   }
 })();
--- build/js/vendor.js
+++ build/js/vendor.js
@@ -1,23 +1,36 @@
 'use strict';
 (function () {
 
+  var pageBody = document.querySelector('.page-body');
   var pageHeader = document.querySelector('.page-header');
 
   pageHeader.classList.remove('page-header--nojs');
   pageHeader.classList.add('page-header--closed');
 
+  var openMenu = function () {
+    pageHeader.classList.remove('page-header--closed');
+    pageHeader.classList.add('page-header--opened');
+    pageBody.classList.add('page-body--fixed');
+  };
+
+  var closeMenu = function () {
+    pageHeader.classList.add('page-header--closed');
+    pageHeader.classList.remove('page-header--opened');
+    pageBody.classList.remove('page-body--fixed');
+  };
+
   var togglePageHeader = function () {
     if (pageHeader.classList.contains('page-header--closed')) {
-      pageHeader.classList.remove('page-header--closed');
-      pageHeader.classList.add('page-header--opened');
+      openMenu();
     } else {
-      pageHeader.classList.add('page-header--closed');
-      pageHeader.classList.remove('page-header--opened');
+      closeMenu();
     }
   };
 
   window.vendor = {
     togglePageHeader: togglePageHeader,
+    openMenu: openMenu,
+    closeMenu: closeMenu,
   };
 
 })();
