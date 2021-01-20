3070b19ae4932575344469fc181c8cc55dc547f7
Bronepuh <darckday2007@mail.ru>
2021-01-20 21:39:58 +0300

    project v.3.0

--- source/js/main.js
+++ source/js/main.js
@@ -1,16 +1,41 @@
 'use strict';
 (function () {
+  var pageBody = document.querySelector('.page-body');
   var pageHeader = document.querySelector('.page-header');
   var headerToggle = document.querySelector('.page-header__toggle');
   var menuItems = document.querySelectorAll('.navigation__item');
 
-  pageHeader.classList.remove('page-header--nojs');
-  pageHeader.classList.add('page-header--closed');
+  if (pageHeader) {
+    pageHeader.classList.remove('page-header--nojs');
+    pageHeader.classList.add('page-header--closed');
+  }
+
+  if (pageHeader && pageBody) {
+    var openMenu = function () {
+      pageHeader.classList.remove('page-header--closed');
+      pageHeader.classList.add('page-header--opened');
+      pageBody.classList.add('page-body--fixed');
+    };
+
+    var closeMenu = function () {
+      pageHeader.classList.add('page-header--closed');
+      pageHeader.classList.remove('page-header--opened');
+      pageBody.classList.remove('page-body--fixed');
+    };
+
+    var togglePageHeader = function () {
+      if (pageHeader.classList.contains('page-header--closed')) {
+        openMenu();
+      } else {
+        closeMenu();
+      }
+    };
+  }
 
   if (headerToggle) {
-    headerToggle.addEventListener('click', window.vendor.togglePageHeader);
+    headerToggle.addEventListener('click', togglePageHeader);
     for (var i = 0; i < menuItems.length; i++) {
-      menuItems[i].addEventListener('click', window.vendor.closeMenu);
+      menuItems[i].addEventListener('click', closeMenu);
     }
   }
 })();
